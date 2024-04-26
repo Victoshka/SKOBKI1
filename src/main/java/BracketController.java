33fa8d185a0ff007c673a4afa5f5fdc4a0d65f5e
Victoshka <ljupka@yandex.ru>
2024-04-26 10:51:02 +0300

    Initial commit

---
+++ src/main/java/BracketController.java
@@ -0,0 +1,36 @@
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Stack;
+
+@RestController
+public class BracketController {
+
+    @PostMapping("/checkBracketBalance")
+    public String checkBracketBalance(@RequestBody String input) {
+        if (isBalanced(input)) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    private boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
